e428c06f85f050c841de1046069f025903791e02
Ivankarpe <aakarpenko2013@gmail.com>
2023-09-23 20:34:21 +0300

    Some restructuring and refactoring

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -1,128 +1,82 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Calculator : MonoBehaviour
 {
-
+    //////////////////////////////////////////////////////////////////////////
+    ///////////////////////   Variables    ///////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
 
     [System.Serializable]
-    public class serializableClass
+    public class ListForInspector ///act like a list, but shown in an inspector
     {
-        public List<double> sampleList = new List<double>();
+        public List<double> list = new List<double>();
     }
+   
+    [SerializeField] private List<double> beta = new List<double>();
+    
     [SerializeField] private List<double> B = new List<double>();
-    [SerializeField] private List<double> B_start = new List<double>();
-    public List<serializableClass> A = new List<serializableClass>();
-    public List<serializableClass> A_start = new List<serializableClass>();
+    
+    public List<ListForInspector> alpha = new List<ListForInspector>();
+    
+    public List<ListForInspector> A = new List<ListForInspector>();
 
     [SerializeField] public List<double> X = new List<double>();
 
     private double mistake;
 
-    public int whatmethodoweusetellmepls = 0;
+    public int method = 0;
     
-    private int itertion_count;
+    private int itertion_count = 0;
 
 
     [SerializeField] GameObject outManager;
-    public void DoShit(int num)
+
+    //////////////////////////////////////////////////////////////////////////
+    ///////////////////////    Start()     ///////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    private void Start()
     {
-        whatmethodoweusetellmepls = num;
-        CalculateThishShit();
+        for (int i = 0; i < 3; i++)
+        {
+            alpha.Add(new ListForInspector());
+
+            A.Add(new ListForInspector());
+
+            beta.Add(default(double));
+
+            B.Add(default(double));
+
+            X.Add(default(double));
+            for (int j = 0; j < 3; j++)
+            {
+                alpha[i].list.Add(0);
+                A[i].list.Add(0);
+            }
+        }
     }
 
-    public void next_previous(int num) {
-     if(!(itertion_count == 0 && num == -1))
+    //////////////////////////////////////////////////////////////////////////
+    ///////////////////////   Procesing Inputs   /////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    public void CangeMethod(int num)
+    {
+        method = num;
+
+        ProcesCalculations();
+    }
+
+    public void ChangeIteration(int num)
+    {
+        if (!(itertion_count == 0 && num == -1))
         {
             itertion_count += num;
         }
-     CalculateThishShit();
-    }
 
-    public void Button(int num)
-    {
-        itertion_count = num;
-        CalculateThishShit();
-    }
-
-    public void CalculateThishShit()
-    {
-        if (whatmethodoweusetellmepls == 0)
-        {
-            itrerationMethod(itertion_count);
-        }
-        else
-        {
-            seidelMethod(itertion_count);
-        }
-
-        for (int i = 0; i < X.Count; i++)
-        {
-            Debug.Log(X[i].ToString());
-        }
-        outManager.GetComponent<outputManageer>().outputNumbers(X);
-        outManager.GetComponent<outputManageer>().outputIteration(itertion_count);
-    }
-
-
-
-
-
-    private void Start()
-    {
-
-        itertion_count = 0;
-
-
-        A.Add(new serializableClass());
-        A.Add(new serializableClass());
-        A.Add(new serializableClass());
-
-        A_start.Add(new serializableClass());
-        A_start.Add(new serializableClass());
-        A_start.Add(new serializableClass());
-
-        A[0].sampleList.Add(0);
-        A[0].sampleList.Add(0);
-        A[0].sampleList.Add(0);
-        A[1].sampleList.Add(0);
-        A[1].sampleList.Add(0);
-        A[1].sampleList.Add(0);
-        A[2].sampleList.Add(0);
-        A[2].sampleList.Add(0);
-        A[2].sampleList.Add(0);
-
-        A_start[0].sampleList.Add(0);
-        A_start[0].sampleList.Add(0);
-        A_start[0].sampleList.Add(0);
-        A_start[1].sampleList.Add(0);
-        A_start[1].sampleList.Add(0);
-        A_start[1].sampleList.Add(0);
-        A_start[2].sampleList.Add(0);
-        A_start[2].sampleList.Add(0);
-        A_start[2].sampleList.Add(0);
-
-
-
-
-        for (int j = 0; j < 3; j++)
-        {
-            B.Add(default(double));
-
-        }
-        for (int j = 0; j < 3; j++)
-        {
-            B_start.Add(default(double));
-
-        }
-        for (int j = 0; j < 3; j++)
-        {
-            X.Add(default(double));
-
-        }
-
+        ProcesCalculations();
     }
 
     public void Getnumber(int x, int y, double number)
@@ -131,31 +85,86 @@ public class Calculator : MonoBehaviour
         {
             mistake = number;
         }
-        if (x==3)
+        if (x == 3)
         {
-            B_start[y] = number;
-            B_start[y] = number;
+            B[y] = number;
         }
         else
         {
-            A_start[x].sampleList[y] = number;
+            A[x].list[y] = number;
         }
     }
 
+    public void Button(int num)
+    {
+        itertion_count = num;
 
-    private List<double> Multi(List<serializableClass> matrix1, List<double> matrix2)
+        ProcesCalculations();
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    /////////////////////////      Output     ////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+
+    public void ProcesCalculations()
+    {
+        List<double> answer;
+
+        if (method == 0)
+        {
+            answer = ItrerationMethod(itertion_count).ToList();
+        }
+        else
+        {
+            answer = SeidelMethod(itertion_count).ToList();
+        }
+
+        for (int i = 0; i < X.Count; i++)
+        {
+            Debug.Log(answer[i].ToString());
+        }
+
+        outManager.GetComponent<outputManageer>().outputNumbers(answer);
+        outManager.GetComponent<outputManageer>().outputIteration(itertion_count);
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    //////////////////////      Calculation      /////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+
+    void transformMatrix()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            double max = A[i].list[i];
+            for (int j = 0; j < 3; j++)
+            {
+                if (i == j)
+                {
+                    alpha[i].list[j] = 0;
+                    continue;
+                }
+
+                alpha[i].list[j] = (0 - A[i].list[j]) / max;
+
+            }
+            beta[i] = B[i] / max;
+            X[i] = B[i] / max;
+        }
+    }
+
+    ////////////////////// Iteration Method /////////////////////////
+    private List<double> Multi(List<ListForInspector> matrix1, List<double> matrix2)
     {
         List<double> result = new List<double>();
 
         for (int i = 0; i < matrix2.Count; i++)
         {
-            result.Add(matrix1[i].sampleList[0] * matrix2[0] + matrix1[i].sampleList[1] * matrix2[1] + matrix1[i].sampleList[2] * matrix2[2]);
+            result.Add(matrix1[i].list[0] * matrix2[0] + matrix1[i].list[1] * matrix2[1] + matrix1[i].list[2] * matrix2[2]);
         }
 
         return result;
     }
-
-
 
     private List<double> Plus(List<double> matrix1, List<double> matrix2)
     {
@@ -167,50 +176,34 @@ public class Calculator : MonoBehaviour
 
         return result;
     }
-    private void itrerationMethod( int Count)
+
+    private List<double> ItrerationMethod( int Count)
     {
         transformMatrix();
 
-
         for (int i = 0; i < Count; i++)
         {
-            X = Plus(B, Multi(A, X));
+            X = Plus(beta, Multi(alpha, X));
         }
 
-
-    }
-    void transformMatrix()
-    {
-        for(int i = 0;i < 3;i++) {
-            double max = A_start[i].sampleList[i];
-            for (int j = 0; j < 3; j++)
-            {
-                if(i==j)
-                {
-                    A[i].sampleList[j] = 0;
-                    continue;
-                }
-                
-                A[i].sampleList[j] = (0-A_start[i].sampleList[j])/ max;
-
-            }
-            B[i] = B_start[i] / max;
-            X[i] = B_start[i] / max;
-        }
+        return X;
     }
 
-    private void seidelMethod(int Count)
+    ////////////////////// Seidel Method /////////////////////////
+    private List<double> SeidelMethod(int Count)
     {
+        
         transformMatrix();
      
         for (int h = 0; h < Count; h++)
         {
             for (int i = 0; i < X.Count; i++)
             {
-                X[i] = B[i] + (A[i].sampleList[0] * X[0] + A[i].sampleList[1] * X[1] + A[i].sampleList[2] * X[2]);
+                X[i] = beta[i] + (alpha[i].list[0] * X[0] + alpha[i].list[1] * X[1] + alpha[i].list[2] * X[2]);
             }
 
         }
 
+        return X;
     }
 }
